ff84a61b9a110ac3ce91b49da3713bea4569ca9d
MiniSony <warrentalex26ies@gmail.com>
2018-08-06 15:58:06 -0600

    restserver - usuario

---
+++ server/config/config.js
@@ -0,0 +1,24 @@
+//=================
+//PUERTO
+//=================
+process.env.PORT = process.env.PORT || 3000;
+
+//=================
+//ENTORNO
+//=================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=================
+//BASE DE DATOS
+//=================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe'
+}else{
+    urlDB = 'mongodb://cafe-user:cafe-user123@ds113942.mlab.com:13942/cafe'
+}
+
+process.env.URLDB = urlDB;
